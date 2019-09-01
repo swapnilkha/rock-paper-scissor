2a341ca659232d63444a621b18eb97dc3bc24f64
swapnilkha <swapnilkha@gmail.com>
2019-08-31 21:30:54 -0400

    commit one

--- rpsapp.js
+++ rpsapp.js
@@ -1,3 +1,4 @@
+//set up our variables
 var userScore = 0;
 var computerScore = 0;
 const userScore_span = document.getElementById("user-score");
@@ -8,12 +9,14 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
 
+//returns a random choice using 1/3 probability of r p or s
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
+//converts the probability to word
 function convertToWord(letter) {
     switch (letter) {
         case 'r':
@@ -33,17 +36,18 @@ function convertToWord(letter) {
 
 function win(userChoice, computerChoice) {
     const userChoice_div = document.getElementById(userChoice);
-    userScore++;
+    userScore++; //if we win increment user score
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
     result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`;
+    //display you win in html
     userChoice_div.classList.add('green-glow');
     setTimeout(function() { document.getElementById(userChoice).classList.remove('green-glow')}, 300);
 }
 
 function lose(userChoice, computerChoice) {
     const userChoice_div = document.getElementById(userChoice);
-    computerScore++;
+    computerScore++; //if user loses, increase computer score
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
     result_p.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}. You lose!`;
@@ -53,12 +57,15 @@ function lose(userChoice, computerChoice) {
 
 function draw(userChoice, computerChoice) {
     const userChoice_div = document.getElementById(userChoice);
+    //if draw, then we print 'draw'
     result_p.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}. Its a draw!`;
     userChoice_div.classList.add('gray-glow');
     setTimeout(function() { document.getElementById(userChoice).classList.remove('gray-glow')}, 300);
 }
 
 function game(userChoice) {
+    //this function takes user and computer choice as input and based on that,
+    //user or computer wins, loses, or draws
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "rs":
@@ -80,6 +87,7 @@ function game(userChoice) {
 }
 
 function main() {
+    //main
     rock_div.addEventListener('click', function() { game('r'); })
     paper_div.addEventListener('click', function() { game('p'); })
     scissor_div.addEventListener('click', function() { game('s'); })
